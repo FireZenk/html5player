bb5f5e6d436a26e8aa1eb9c86850f52f83fb7907
FireZenk <firezenk@gmail.com>
2013-07-23 12:29:32 +0200

    Version 1.6
    Added Wavesurfer and code inprovement

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Vesper: HTML5 Audio player
 ===========
 Vesper HTML5 audio player is an audio player completely based on HTML5.
 
-![alt text](http://img835.imageshack.us/img835/8994/vesper131.png)
+![alt text](http://img203.imageshack.us/img203/4329/6bsh.jpg)
 
 Implements the new features of audio label and also automatically download cover art of albums.
 
--- js/vesper.js
+++ js/vesper.js
@@ -2,99 +2,119 @@
  * Vesper.js 
  *
  * @project Vesper
- * @version 1.5
+ * @version 1.6
  * @author Jorge Garrido Oval, firezenk@gmail.com
  * @copyright 2013
  * @license MIT
  */
-var lastfmAPIkey="bf7528310d7bdbb8584e57244cdb549b";
-
-var zPlayer=document.getElementById("player");
-
-var isPlaying=false;
-
-function zMute() {
-  zPlayer.muted=!zPlayer.muted;
+function zMute(){
+  zPlayer.muted=!zPlayer.muted
 }
 
-function zPlayPause() {
-  if(isPlaying) {
+function zPlayPause(){
+  if(isPlaying){
     document.getElementById("play").innerHTML="<icon>play</icon>";
-    zPlayer.pause();
+    zPlayer.pause()
   } else {
     document.getElementById("play").innerHTML="<icon>pause</icon>";
-    zPlayer.play();
+    zPlayer.play()
   }
-  isPlaying=!isPlaying;
+  wavesurfer.playPause();
+  isPlaying=!isPlaying
 }
 
-function zStop() {
+function zStop(){
   document.getElementById("play").innerHTML="<icon>play</icon>";
   zPlayer.pause();
   zPlayer.currentTime=0;
   isPlaying=false;
-  zTimeUpdate();
+  zTimeUpdate()
 }
 
-function zVolumeUp() {
-  zPlayer.volume+=.1;
+function zVolumeUp(){
+  zPlayer.volume+=.1
 }
 
-function zVolumeDown() {
-  zPlayer.volume-=.1;
+function zVolumeDown(){
+  zPlayer.volume-=.1
 }
 
-function zTimeUpdate() {
+function zTimeUpdate(){
   document.getElementById("currenttime").innerHTML=convertTime(Math.floor(zPlayer.currentTime));
   document.getElementById("totaltime").innerHTML=convertTime(Math.floor(zPlayer.duration));
   document.getElementById("time").value=Math.floor(zPlayer.currentTime);
   document.getElementById("time").max=Math.floor(zPlayer.duration);
   
   if(zPlayer.currentTime>=zPlayer.duration)
-    zAlbumArt();
-}
+    zAlbumArt()
+  }
 
-function convertTime(sec) {
-  var hrs = Math.floor(sec/3600);
-  var min = Math.floor((sec%3600)/60);
-  sec = sec % 60;
-  if(sec<10) sec = "0" + sec;
-  if(min<10) min = "0" + min;
-  if(hrs!=0) return hrs + ":" + min + ":" + sec;
-  else return min + ":" + sec;
-}
+  function convertTime(e){
+    var t=Math.floor(e/3600);
+    var n=Math.floor(e%3600/60);
+    e=e%60;
 
-function zAlbumArt() {
+    if(e<10)  e="0"+e;
+    if(n<10)  n="0"+n;
+    if(t!=0)  return t+":"+n+":"+e;
+    else      return n+":"+e
+  }
 
-  var currentArtist = zPlayer.getAttribute('data-artist');
-
-  var currentAlbum = zPlayer.getAttribute('data-album');
-
-  var url = "http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key="+
-    lastfmAPIkey+"&artist="+currentArtist+"&album="+currentAlbum;
-
-  var xmlhttp;
-  var txt,x,xx,i;
-
-  if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-  else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-
-  xmlhttp.onreadystatechange = function() {
-
-    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-      x = xmlhttp.responseXML.documentElement.getElementsByTagName("album");
-
-      for (i = 0; i<x.length; i++) {
-        xx = x[i].getElementsByTagName("image");
-          
+function zAlbumArt(){
+  var e=zPlayer.getAttribute("data-artist");
+  var t=zPlayer.getAttribute("data-album");
+  var n="http://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key="+lastfmAPIkey+"&artist="+e+"&album="+t;
+  var r,i,s,o,u;
+  if(window.XMLHttpRequest)
+    r=new XMLHttpRequest;
+  else 
+    r=new ActiveXObject("Microsoft.XMLHTTP");
+  r.onreadystatechange=function(){
+    if(r.readyState==4&&r.status==200){
+      s=r.responseXML.documentElement.getElementsByTagName("album");
+      for(u=0;u<s.length;u++){
+        o=s[u].getElementsByTagName("image");
         try {
-          txt = "<div style='background-image: url("+ xx[1].firstChild.nodeValue +");background-repeat: no-repeat;'></div>";
-        } catch (er) {}
+          i="<div style='background-image: url("+o[1].firstChild.nodeValue+");background-repeat: no-repeat;'></div>"
+        } catch(e){ }
       }
-      document.getElementById('art').innerHTML = txt;
+      document.getElementById("art").innerHTML=i
     }
   };
 
-  xmlhttp.open("GET",url,true);
-  xmlhttp.send();
+  r.open("GET",n,true);
+  r.send()
 }
+
+var lastfmAPIkey="bf7528310d7bdbb8584e57244cdb549b";
+var zPlayer=document.getElementById("player");
+var filename = document.getElementById('player').getAttribute('src');
+var isPlaying=false;
+var wavesurfer=Object.create(WaveSurfer);
+
+document.addEventListener("DOMContentLoaded",function(){
+  wavesurfer.init({
+    container:document.querySelector(".waveform"),
+    fillParent:true,
+    markerColor:"rgba(0, 0, 0, 0.5)",
+    frameMargin:.1,
+    maxSecPerPx:parseFloat(location.hash.substring(1)),
+    loadPercent:true,
+    waveColor:"darkgray",
+    progressColor:"whitesmoke",
+    loaderColor:"transparent",
+    cursorWidth:0
+  });
+  wavesurfer.load(document.getElementById("player").src)
+});
+
+ID3.loadTags(filename, function() {
+  var tags = ID3.getAllTags(filename);
+
+  document.getElementById('artist').innerHTML = "Artist: "+tags.artist;
+  document.getElementById('player').setAttribute('data-artist', tags.artist);
+  document.getElementById('album').innerHTML = "Album: "+tags.album;
+  document.getElementById('player').setAttribute('data-album', tags.album);
+
+  zAlbumArt();
+},{tags: ["artist","album"]})
